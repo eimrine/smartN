d4de2b3bb7140176fd5c63db5aa07c83c7cdefa3
Eimrine's Point of View <eimrine@users.noreply.github.com>
2014-11-06 01:54:53 +0200

    Update views.py
    This is a new version of our view function login which validates and saves form's data.

--- app/views.py
+++ app/views.py
@@ -22,6 +22,9 @@ def index():
 @app.route('/login', methods = ['GET', 'POST'])
 def login():
     form = LoginForm()
+    if form.validate_on_submit():
+        flash('Login requested for OpenID="' + form.openid.data + '", remember_me=' + str(form.remember_me.data))
+        return redirect('/index')
     return render_template('login.html', 
         title = 'Sign In',
         form = form)
