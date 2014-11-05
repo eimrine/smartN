5f4a5f4dbc1cdb76d884f20df1242bc0dc2f72aa
Eimrine's Point of View <eimrine@users.noreply.github.com>
2014-11-06 01:19:57 +0200

    Update views.py
    create some fake users and some writings for view

--- app/views.py
+++ app/views.py
@@ -1,10 +1,16 @@
-from flask import render_template
-from app import app
-
-@app.route('/')
-@app.route('/index')
 def index():
     user = { 'nickname': 'Miguel' } # выдуманный пользователь
+    posts = [ # список выдуманных постов
+        { 
+            'author': { 'nickname': 'John' }, 
+            'body': 'Beautiful day in Portland!' 
+        },
+        { 
+            'author': { 'nickname': 'Susan' }, 
+            'body': 'The Avengers movie was so cool!' 
+        }
+    ]
     return render_template("index.html",
         title = 'Home',
-        user = user)
+        user = user,
+        posts = posts)
