1e19473f941046c8aed57d976d99c8c173d54401
Eimrine's Point of View <eimrine@users.noreply.github.com>
2014-11-06 01:00:15 +0200

    Update views.py
    using template in view function

--- app/views.py
+++ app/views.py
@@ -1,6 +1,10 @@
+from flask import render_template
 from app import app
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, World!"
+    user = { 'nickname': 'Miguel' } # выдуманный пользователь
+    return render_template("index.html",
+        title = 'Home',
+        user = user)
