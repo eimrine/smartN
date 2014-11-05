1c371880320fbd3333b5a1998d78bd68153131fa
Eimrine's Point of View <eimrine@users.noreply.github.com>
2014-11-06 01:52:27 +0200

    Update views.py
    tsis is the last step before we can see our form.
    we imported LoginForm, created its ex and send it in template. all of it we need to draw fields of form.
    dont care about import flash and redirect, today thea are useless for me/

--- app/views.py
+++ app/views.py
@@ -1,3 +1,7 @@
+from flask import render_template, flash, redirect
+from app import app
+from forms import LoginForm
+
 def index():
     user = { 'nickname': 'Miguel' } # выдуманный пользователь
     posts = [ # список выдуманных постов
@@ -14,3 +18,10 @@ def index():
         title = 'Home',
         user = user,
         posts = posts)
+
+@app.route('/login', methods = ['GET', 'POST'])
+def login():
+    form = LoginForm()
+    return render_template('login.html', 
+        title = 'Sign In',
+        form = form)
