ae0fe1d5aee80a60ddb0cd65dcb756d66b3c5d3b
Eimrine's Point of View <eimrine@users.noreply.github.com>
2014-11-08 00:28:15 +0200

    Update prepare.sh
    hope this works

--- prepare.sh
+++ prepare.sh
@@ -3,28 +3,28 @@
 #
 # you need bash and python 2
 # also, you need folder "smartn" in which this script will install our server
-#
+# finally please, go in this folder and run this
 #
 # BUILD ENVIROMENT
 #
 sudo apt-get install python-virtualenv
-python virtualenv.py flask # this will create full enviroment inside "flask" folder
+virtualenv flask # this will create full enviroment inside "flask" folder
 #
 #
 # INSTALL FLASK
 #
-flask/bin/pip install flask==0.9
+flask/bin/pip install flask
 flask/bin/pip install flask-login
 flask/bin/pip install flask-openid
 flask/bin/pip install flask-mail
-flask/bin/pip install sqlalchemy==0.7.9
-flask/bin/pip install flask-sqlalchemy==0.16
+flask/bin/pip install flask-sqlalchemy
 flask/bin/pip install sqlalchemy-migrate
-flask/bin/pip install flask-whooshalchemy==0.54a
-flask/bin/pip install flask-wtf==0.8.4
-flask/bin/pip install pytz==2013b
-flask/bin/pip install flask-babel==0.8
-flask/bin/pip install flup
+flask/bin/pip install flask-whooshalchemy
+flask/bin/pip install flask-wtf
+flask/bin/pip install flask-babel
+flask/bin/pip install guess_language
+flask/bin/pip install flipflop
+flask/bin/pip install coverage
 ## rm virtualenv.py TODO
 #
 #
